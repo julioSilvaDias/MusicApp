a2ff66098a7149ef6c8bc012e3cb60236cbaf8d0
Julio Silva <juliocesar.silvadi@elorrieta-errekamari.com>
2024-09-26 17:15:43 +0200

    MusicApp
    -Vista terminada tanto vertical como horizontal

--- MusicApp/app/src/main/java/com/example/musicapp/MainActivity.kt
+++ MusicApp/app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.musicapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,20 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val botonPause = findViewById<ImageButton>(R.id.buttonPause)
+        val botonPlay = findViewById<ImageButton>(R.id.botonPlay)
+
+        findViewById<ImageButton>(R.id.botonPlay).setOnClickListener {
+            botonPlay.visibility = View.INVISIBLE
+            botonPause.visibility = View.VISIBLE
         }
+
+        findViewById<ImageButton>(R.id.buttonPause).setOnClickListener {
+            botonPlay.visibility = View.VISIBLE
+            botonPause.visibility = View.INVISIBLE
+        }
+
     }
 }
